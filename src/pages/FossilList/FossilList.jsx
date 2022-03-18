4c2a366a41f91f55f572799aa357e862c6389d8f
Tucker <tuckerchaisit@gmail.com>
2022-03-17 23:09:49 -0400

    implement api call

---
+++ src/pages/FossilList/FossilList.jsx
@@ -0,0 +1,21 @@
+import Fossil from "../../components/Fossil/Fossil";
+import { useState, useEffect } from "react";
+import { getFossils } from "../../services/api-call";
+
+const FossilList = (props) => {
+  const [fossils, setFossils] =useState()
+  
+  useEffect(()=> {
+    getFossils()
+    .then(fossilData => console.log(fossilData))
+  },[])
+
+  return ( 
+    <>
+    <h2>FOssil List Page</h2>
+    <Fossil />
+    </>
+   );
+}
+ 
+export default FossilList;
